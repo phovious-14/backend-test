d986aaf7711cf76967da020a4eac2990a64881a4
phovious-14 <krishnpanchal14@gmail.com>
2024-09-07 10:43:20 +0530

    added grid style in datapoint components

--- src/components/DataPoints.tsx
+++ src/components/DataPoints.tsx
@@ -3,7 +3,7 @@ import { beckettCardDataType, cgcCardDataType, psaCardDataType } from "@/lib/typ
 export default function DataPoints({ data }: { data: psaCardDataType | cgcCardDataType | beckettCardDataType }) {
 
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {Object.entries(data).map(([key, value]) => (
         <div key={key}>
           <strong>{key}:</strong> {String(value)}
